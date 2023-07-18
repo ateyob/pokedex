84de51f8497584da8799664f0410787bf0b96baf
Eyob <ateyob@gmail.com>
2023-07-18 08:11:01 +0300

    truncate

--- app/components/pokemon-grid.tsx
+++ app/components/pokemon-grid.tsx
@@ -64,24 +64,67 @@ export function PokemonGrid({ pokemonList }: PokemonGridProps) {
 
       <div className="flex justify-center md:mt-16 overflow-x-auto">
   <ul className="flex flex-wrap flex-row gap-4 justify-center">
-    {Array(Math.ceil(filteredPokemonList.length / itemsPerPage))
-      .fill(0)
-      .map((_, index) => (
+    {currentPage > 1 && (
+      <>
         <li
-          key={index}
-          className={`mr-1 ${
-            currentPage === index + 1
-              ? "bg-purple-500 text-white"
-              : "bg-gray-200 hover:bg-purple-300"
-          } rounded-full px-4 py-2 text-black cursor-pointer font-semibold`}
-          onClick={() => handlePageChange(index + 1)}
+          className="mr-1 bg-gray-200 rounded-full px-4 py-2 text-black cursor-pointer font-semibold"
+          onClick={() => handlePageChange(currentPage - 1)}
         >
-          {index + 1}
+          &larr;
         </li>
-      ))}
+        <li
+          className={`mr-1 ${
+            currentPage === 1 ? "bg-purple-500 text-white" : "bg-gray-200 hover:bg-purple-300"
+          } rounded-full px-4 py-2 text-black cursor-pointer font-semibold`}
+          onClick={() => handlePageChange(1)}
+        >
+          1
+        </li>
+      </>
+    )}
+    {Array(Math.ceil(filteredPokemonList.length / itemsPerPage)).fill(0).map((_, index) => {
+      if (
+        index === currentPage - 1 ||
+        index === currentPage ||
+        index === currentPage + 1 ||
+        index === Math.ceil(filteredPokemonList.length / itemsPerPage) - 1
+      ) {
+        if (index === currentPage + 1 && index !== 2) {
+          return (
+            <li
+              key={index}
+              className="mr-1 bg-gray-200 rounded-full px-4 py-2 text-black cursor-pointer font-semibold"
+            >
+              ...
+            </li>
+          );
+        }
+        return (
+          <li
+            key={index}
+            className={`mr-1 ${
+              currentPage === index + 1
+                ? "bg-purple-500 text-white"
+                : "bg-gray-200 hover:bg-purple-300"
+            } rounded-full px-4 py-2 text-black cursor-pointer font-semibold`}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </li>
+        );
+      }
+      return null;
+    })}
+    {currentPage < Math.ceil(filteredPokemonList.length / itemsPerPage) && (
+      <li
+        className="mr-1 bg-gray-200 rounded-full px-4 py-2 text-black cursor-pointer font-semibold"
+        onClick={() => handlePageChange(currentPage + 1)}
+      >
+        &rarr;
+      </li>
+    )}
   </ul>
 </div>
-
 
     </>
   );
